35d71ae68cdc61d56607eebc9f18ec18e1a92795
Roseane R. Silva <roseane@wustl.edu>
2023-08-14 22:10:53 -0500

    New python tool to analyze and plot fit results

--- analyzetool/__init__.py
+++ analyzetool/__init__.py
@@ -5,7 +5,7 @@ rdkit_loader = importlib.find_loader('rdkit')
 found = rdkit_loader is not None
 
 if found:
-    from . import gas, liquid, process, prmedit, auxtinker, auxfitting, run_sim, xyz2mol
+    from . import gas, liquid, process, prmedit, auxtinker, auxfitting, run_sim, xyz2mol, analyze_fit
 else:
     from . import gas, liquid, process, prmedit, auxtinker, auxfitting, run_sim
 
